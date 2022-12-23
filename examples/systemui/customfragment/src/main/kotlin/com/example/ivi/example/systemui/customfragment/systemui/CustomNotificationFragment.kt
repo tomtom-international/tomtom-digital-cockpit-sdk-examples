91b5a76215d32e25e16833fe34fa557d41861fd2
Digital Cockpit Publishing App <ivi_archipelago@groups.tomtom.com>
2022-12-23 06:37:02 +0000

    Digital Cockpit SDK Release 1.0.4668-2320

---
+++ examples/systemui/customfragment/src/main/kotlin/com/example/ivi/example/systemui/customfragment/systemui/CustomNotificationFragment.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2022 TomTom NV. All rights reserved.
+ *
+ * This software is the proprietary copyright of TomTom NV and its subsidiaries and may be
+ * used for internal evaluation purposes or commercial use strictly subject to separate
+ * license agreement between you and TomTom NV. If you are the licensee, you are only permitted
+ * to use this software in accordance with the terms of your license agreement. If you are
+ * not the licensee, you are not authorized to use this software in any manner and should
+ * immediately return or destroy it.
+ */
+
+package com.example.ivi.example.systemui.customfragment.systemui
+
+import com.example.ivi.example.systemui.customfragment.databinding.TtiviCustomfragmentNotificationfragmentBinding
+import com.tomtom.ivi.platform.frontend.api.common.frontend.IviFragment
+import com.tomtom.ivi.platform.frontend.api.template.notificationpanel.stock.StockNotificationPanel
+import com.tomtom.ivi.platform.frontend.api.template.notificationpanel.stock.StockNotificationPanel.StockNotificationViewModel
+
+/**
+ * This fragment replaces the normal fragment of the [StockNotificationPanel].
+ *
+ * In this example we chose to use the regular view model of the panel, the
+ * [StockNotificationViewModel], since this allowed us to see what the notification is about. This
+ * is no requirement though, it could just as well be a sub class of the normal view model, or a
+ * different class altogether. The regular view model of the package might even be declared as
+ * package-private, then you'll have to create a new view model.
+ */
+internal class CustomNotificationFragment : IviFragment<StockNotificationPanel, StockNotificationViewModel>(
+    StockNotificationViewModel::class
+) {
+    override val viewFactory: ViewFactory<*> =
+        ViewFactory(TtiviCustomfragmentNotificationfragmentBinding::inflate)
+}
--- oss-compliancy-report.md
+++ oss-compliancy-report.md
@@ -1,4 +1,4 @@
-# OSS report for TomTom _Digital Cockpit SDK_ version 1.0.4665-2317
+# OSS report for TomTom _Digital Cockpit SDK_ version 1.0.4668-2320
 
 TomTom will provide on request the source code of software licensed under GPL, LGPL and certain other open source licenses where this is a requirement of the applicable license terms.
 
